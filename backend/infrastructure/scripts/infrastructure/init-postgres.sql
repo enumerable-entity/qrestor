9e88c2aa808258324f9a3ee30755364cf54482d4
Enumerable-entity <alfred.dc.alpha@gmail.com>
2024-01-13 23:24:03 +0100

    project infrastructure config

--- backend/infrastructure/scripts/infrastructure/init-postgres.sql
+++ backend/infrastructure/scripts/infrastructure/init-postgres.sql
@@ -1,5 +1,4 @@
--- Postgres user initialization script fro each microservice
-
 create user qresolver with encrypted password 'qresolver';
 create schema if not exists qresolver;
 grant all privileges on schema qresolver to qresolver;
+
