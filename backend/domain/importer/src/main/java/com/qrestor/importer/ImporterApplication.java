56c0d82441bbceeb9916d1d357b8f677b6c16da7
Enumerable-entity <alfred.dc.alpha@gmail.com>
2024-02-02 00:53:04 +0100

    import/export initial setup

--- backend/domain/importer/src/main/java/com/qrestor/importer/ImporterApplication.java
+++ backend/domain/importer/src/main/java/com/qrestor/importer/ImporterApplication.java
@@ -2,8 +2,14 @@ package com.qrestor.importer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.qrestor"})
 public class ImporterApplication {
 
     public static void main(String[] args) {
---
+++ backend/domain/importer/src/main/java/com/qrestor/importer/security/SpringSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.qrestor.importer.security;
+
+import com.qrestor.importer.RestEndpoints;
+import com.qrestor.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(RestEndpoints.SERVICE_ENDPOINTS_LIST).permitAll()
+                                .requestMatchers(RestEndpoints.PUBLIC_ENDPOINTS_LIST).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
