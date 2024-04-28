fedbc798018ccaec548a89ffd02097ee3f0e041b
Enumerable-entity <alfred.dc.alpha@gmail.com>
2024-04-28 16:10:47 +0200

    Revert "Update App.vue"
    This reverts commit 05f8047d9e8c6bdc6eccba162e4d41d0140447fc.

--- frontend/qrestor/src/App.vue
+++ frontend/qrestor/src/App.vue
@@ -1,10 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1> MAINTANCE </h1>
-  <p> We are currently under maintance. Please check back later. </p>
-  <p> Sorry for the inconvenience. </p>
-  <p> - The Management </p>
+  <RouterView> </RouterView>
 </template>
 
 <style scoped></style>
